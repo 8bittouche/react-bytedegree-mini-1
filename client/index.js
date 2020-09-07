cb8a508a02d0a17de753a8381e8029e31aecd521
8bittouche <8bittouche@gmail.com>
2020-09-07 13:33:31 +0900

    2020-09-07 client/index.js 1차 리뷰 반영

--- client/index.js
+++ client/index.js
@@ -1,7 +1,4 @@
 function onClickLi(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
     // 자동완성 단어 클릭 시 검색창에 해당 단어 표시
     const searchField = document.querySelector('#search-field');
     searchField.value = e.target.innerHTML;
@@ -10,27 +7,17 @@ function onClickLi(e) {
 function showAutocompleteWords(words) {
     const autocompleteList = document.querySelector('.autocomplete-results');
 
-    // ul tag의 모든 자식 노드 제거
-    while(autocompleteList.firstChild) {
-        autocompleteList.removeChild(autocompleteList.firstChild);
-    }
+    // 자동완성 단어 리스트 클릭 이벤트 등록
+    autocompleteList.addEventListener('click', onClickLi);
 
-    // ul tag에 자동완성 단어 추가 (li tag)
-    words.forEach(word => {
-        const li = document.createElement('li');
-        li.innerHTML = word;
-        li.onclick = onClickLi;
-        autocompleteList.appendChild(li);
-    });
+    // 자동완성 단어 리스트 보여주기
+    autocompleteList.innerHTML = words.map(word => `<li>${word}</li>`).join("");
 }
 
-async function onKeyUp(e) {
-    e.preventDefault();
-    e.stopPropagation();
-
+function onKeyUp(e) {
     // 서버에서 자동완성 단어 가져오기
     const word = e.target.value;
-    await fetch(`http://localhost:3000/autocomplete?keyword=${word}`)
+    fetch(`http://localhost:3000/autocomplete?keyword=${word}`)
     .then(res => {
         return res.json();
     })
@@ -43,7 +30,7 @@ async function onKeyUp(e) {
 function main() {
     // 검색창 input에 onkeyup 함수 추가 (검색창의 단어가 바뀔때마다 호출)
     const searchField = document.querySelector('#search-field');
-    searchField.onkeyup = onKeyUp;
+    searchField.addEventListener('keyup', onKeyUp);
 }
 
 document.addEventListener('DOMContentLoaded', main);
